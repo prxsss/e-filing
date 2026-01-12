34977db6e4d6f472c5974181861486be4743a330
prxsss <9phuriphat.k@gmail.com>
2026-01-12 20:58:57 +0700

    chore: integrate antfu eslint config with existing eslint setup

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,21 +3,17 @@ import tailwindcss from '@tailwindcss/vite';
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
 
-	modules: ['@nuxt/eslint', '@nuxt/ui'],
+  modules: ['@nuxt/eslint', '@nuxt/ui'],
 
-	devtools: { enabled: true },
-	css: ['./app/assets/css/main.css'],	compatibilityDate: '2025-07-15',
-	vite: {
-		plugins: [tailwindcss()],
-	},
-	eslint: {
-		config: {
-			stylistic: {
-				semi: true,
-				quotes: 'single',
-				commaDangle: 'always-multiline',
-				indent: 'tab',
-			},
-		},
-	},
+  devtools: { enabled: true },
+  css: ['./app/assets/css/main.css'],
+  compatibilityDate: '2025-07-15',
+  eslint: {
+    config: {
+      standalone: false,
+    },
+  },
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
