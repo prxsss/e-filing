03c6b426527381ab6eb921c3378e88547c0289a2
jatnipit <jatnipt.k@ku.th>
2026-01-16 12:17:19 +0700

    move pages to student folder

---
+++ app/pages/student/dashboard.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+definePageMeta({
+  title: 'dashboard',
+});
+</script>
+
+<template>
+  <div class="p-4">
+    <h1 class="text-2xl font-bold mb-4">
+      Welcome to the Dashboard
+    </h1>
+    <p>This is the main content area.</p>
+  </div>
+</template>
