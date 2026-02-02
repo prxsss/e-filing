6c10c0e6a4e0b74aedf21c0870b29193faf87e8e
Phantharatt <156408508+Phantharatt@users.noreply.github.com>
2026-02-03 02:41:33 +0700

    have add , edit , delete to field that using real data from database
    feat: implement CRUD operations for template fields and add field creation modal

--- lib/db/schema/request-template-fields.ts
+++ lib/db/schema/request-template-fields.ts
@@ -1,9 +1,16 @@
-import { bigint, pgTable, text, timestamp } from 'drizzle-orm/pg-core';
+import { bigint, boolean, integer, pgTable, text, timestamp } from 'drizzle-orm/pg-core';
 
 export const requestTemplateFields = pgTable('request_template_fields', {
   id: bigint('id', { mode: 'number' }).primaryKey().generatedByDefaultAsIdentity(),
-  templateId: bigint('template_id', { mode: 'number' }),
-  fieldName: text('field_name'),
-  fieldType: text('field_type'),
   createdAt: timestamp('created_at', { withTimezone: true }).notNull().defaultNow(),
+  name: text('name').notNull(),
+  type: text('type').notNull(),
+  label: text('label').notNull(),
+  font: text('font'),
+  fontSize: integer('font_size'),
+  isFillable: boolean('is_fillable').default(true),
+  width: integer('width').notNull(),
+  height: integer('height').notNull(),
+  icon: text('icon').notNull(),
+  amount: integer('amount').default(1),
 });
