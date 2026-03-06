588fa6a381b17c8ca8445c6839b381e57e48d47c
prxsss <9phuriphat.k@gmail.com>
2026-03-06 15:08:08 +0700

    feat: update session cookie maxAge to 1 day

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -21,7 +21,9 @@ export default defineNuxtConfig({
       password: env.NUXT_SESSION_PASSWORD,
       name: 'e-filing-session',
       cookie: {
-        maxAge: 60 * 60 * 24 * 7, // 7 days
+        // maxAge: 60 * 60 * 24 * 7, // 7 days
+        maxAge: 60 * 60 * 24, // 1 day
+        // maxAge: 60 * 60, // 1 hour
       },
     },
   },
