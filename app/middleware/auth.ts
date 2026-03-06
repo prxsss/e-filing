03eb04892506c70e37b91abcc5ea33330c227a1d
prxsss <9phuriphat.k@gmail.com>
2026-03-06 16:32:44 +0700

    refactor: remove unused authentication and guest middleware files

--- app/middleware/auth.ts
+++
@@ -1,7 +0,0 @@
-export default defineNuxtRouteMiddleware(() => {
-  const session = useUserSession();
-  const localPath = useLocalePath();
-  if (!session.loggedIn.value) {
-    return navigateTo(localPath('/login'));
-  }
-});
