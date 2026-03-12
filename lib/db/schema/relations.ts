cd2a571b6c83449188712a32e463425870d52295
prxsss <9phuriphat.k@gmail.com>
2026-03-12 15:59:51 +0700

    refactor: update database schema by pulling from supabase

---
+++ lib/db/schema/relations.ts
@@ -0,0 +1,77 @@
+import { relations } from "drizzle-orm/relations";
+import { faculties, users, request, signatureFlow, roles, attachments, requestTemplateValues, permissions, rolePermissions, userRoles } from "./schema";
+
+export const usersRelations = relations(users, ({one, many}) => ({
+	faculty: one(faculties, {
+		fields: [users.facultyId],
+		references: [faculties.id]
+	}),
+	userRoles: many(userRoles),
+}));
+
+export const facultiesRelations = relations(faculties, ({many}) => ({
+	users: many(users),
+}));
+
+export const signatureFlowRelations = relations(signatureFlow, ({one}) => ({
+	request: one(request, {
+		fields: [signatureFlow.requestId],
+		references: [request.id]
+	}),
+	role: one(roles, {
+		fields: [signatureFlow.roleId],
+		references: [roles.id]
+	}),
+}));
+
+export const requestRelations = relations(request, ({many}) => ({
+	signatureFlows: many(signatureFlow),
+	attachments: many(attachments),
+	requestTemplateValues: many(requestTemplateValues),
+}));
+
+export const rolesRelations = relations(roles, ({many}) => ({
+	signatureFlows: many(signatureFlow),
+	rolePermissions: many(rolePermissions),
+	userRoles: many(userRoles),
+}));
+
+export const attachmentsRelations = relations(attachments, ({one}) => ({
+	request: one(request, {
+		fields: [attachments.requestId],
+		references: [request.id]
+	}),
+}));
+
+export const requestTemplateValuesRelations = relations(requestTemplateValues, ({one}) => ({
+	request: one(request, {
+		fields: [requestTemplateValues.requestId],
+		references: [request.id]
+	}),
+}));
+
+export const rolePermissionsRelations = relations(rolePermissions, ({one}) => ({
+	permission: one(permissions, {
+		fields: [rolePermissions.permissionId],
+		references: [permissions.id]
+	}),
+	role: one(roles, {
+		fields: [rolePermissions.roleId],
+		references: [roles.id]
+	}),
+}));
+
+export const permissionsRelations = relations(permissions, ({many}) => ({
+	rolePermissions: many(rolePermissions),
+}));
+
+export const userRolesRelations = relations(userRoles, ({one}) => ({
+	role: one(roles, {
+		fields: [userRoles.roleId],
+		references: [roles.id]
+	}),
+	user: one(users, {
+		fields: [userRoles.userId],
+		references: [users.id]
+	}),
+}));
